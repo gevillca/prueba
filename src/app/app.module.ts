dfda267e593c2d234cafe9ddc9956b6586da2d5d
gevillca <gevillca@gmail.com>
2019-09-11 13:39:40 -0400

    prieba12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PruebaComponent } from './compoents/prueba/prueba.component';
+import { Prueba1Component } from './compoents/prueba1/prueba1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
+    PruebaComponent,
+    Prueba1Component
   ],
   imports: [
     BrowserModule

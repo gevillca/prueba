08492162f5052860904ca360a705ac278590f211
gevillca <gevillca@gmail.com>
2019-09-11 14:05:31 -0400

    mmm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PruebaComponent } from './compoents/prueba/prueba.component';
 import { Prueba1Component } from './compoents/prueba1/prueba1.component';
+import { NewComponent } from './components/new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PruebaComponent,
-    Prueba1Component
+    Prueba1Component,
+    NewComponent
   ],
   imports: [
     BrowserModule
